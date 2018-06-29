2110c92b70584f39d9588b72509f265261303af4
Angela Xiao <angelaxiao@fb.com>
2018-06-29 15:43:41 -0700

    Butterknife implemented

--- app/src/main/java/peapod/angela/flixster/MovieDetailsActivity.java
+++ app/src/main/java/peapod/angela/flixster/MovieDetailsActivity.java
@@ -1,13 +1,17 @@
 package peapod.angela.flixster;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import org.parceler.Parcels;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import peapod.angela.flixster.models.Movie;
 
 public class MovieDetailsActivity extends AppCompatActivity {
@@ -15,21 +19,18 @@ public class MovieDetailsActivity extends AppCompatActivity {
     // the movie to display
     Movie movie;
 
-    // the view objects
-    TextView tvTitle;
-    TextView tvOverview;
-    RatingBar rbVoteAverage;
-    TextView popularity;
+    // butterknife, track view objects
+    @Nullable @BindView(R.id.ivPosterImage) ImageView ivPosterImage;
+    @Nullable @BindView(R.id.ivBackdropImage) ImageView ivBackdropImage;
+    @BindView(R.id.tvTitle) TextView tvTitle;
+    @BindView(R.id.tvOverview) TextView tvOverview;
+    @BindView(R.id.rbVoteAverage) RatingBar rbVoteAverage;
+    @BindView(R.id.popularity) TextView popularity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
-        // resolve the view objects
-        tvTitle = (TextView) findViewById(R.id.tvTitle);
-        tvOverview = (TextView) findViewById(R.id.tvOverview);
-        rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
-        popularity = (TextView) findViewById(R.id.popularity);
+        ButterKnife.bind(this);
 
         // unwrap the movie passed in via intent, using its simple name as a key
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
@@ -43,5 +44,5 @@ public class MovieDetailsActivity extends AppCompatActivity {
         // vote average is 0..10, convert to 0..5 by dividing by 2
         float voteAverage = movie.getVoteAverage().floatValue();
         rbVoteAverage.setRating(voteAverage = voteAverage > 0 ? voteAverage / 2.0f : voteAverage);
-        }
+    }
 }
